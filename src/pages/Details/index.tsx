0b71ea026b8919ac49d6fd3a36d299bf2206144f
Mateus Leonardo <m4teusmatos@gmail.com>
2022-05-07 10:23:10 -0300

    :rocket: creating page details

---
+++ src/pages/Details/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Container from '../../components/Container';
+import {Main, Header} from './styles';
+
+const Details = () => {
+  return (
+    <Container>
+      <Main>
+        <Header />
+      </Main>
+    </Container>
+  );
+};
+
+export default Details;
---
+++ src/pages/Details/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const Main = styled.View`
+  flex: 1;
+  padding: 20px 30px 20px 30px;
+  background-color: #fafafa;
+`;
+
+export const Header = styled.View`
+  width: 100%;
+`;
